a5487f532dbbdc21cc76189b3327c96dba537e2f
yushia4a <61086868+yushia4a@users.noreply.github.com>
2020-03-23 09:18:47 +0900

    add files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+$(function(){
+  $('a[href^=#]').click(function(){
+      var speed = 800;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+  });
+
+});
